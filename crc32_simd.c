7d25a97213f3564ab667100d12b43099602c9316
Septa2112 <jia3.liu@intel.com>
2022-11-07 23:11:07 +0800

    avx512

--- crc32_simd.c
+++ crc32_simd.c
@@ -19,10 +19,10 @@
  */
 
 #include <emmintrin.h>
-#include <smmintrin.h>
+#include <immintrin.h>
 #include <wmmintrin.h>
 
-uint32_t ZLIB_INTERNAL crc32_sse42_simd_(  /* SSE4.2+PCLMUL */
+uint32_t ZLIB_INTERNAL crc32_sse42_simd_(  /* avx512+vpclmulqdq */
     const unsigned char *buf,
     z_size_t len,
     uint32_t crc)
@@ -36,19 +36,19 @@ uint32_t ZLIB_INTERNAL crc32_sse42_simd_(  /* SSE4.2+PCLMUL */
     static const uint64_t zalign(16) k5k0[] = { 0x0163cd6124, 0x0000000000 };
     static const uint64_t zalign(16) poly[] = { 0x01db710641, 0x01f7011641 };
 
-    __m128i x0, x1, x2, x3, x4, x5, x6, x7, x8, y5, y6, y7, y8;
+    __m512i x0, x1, x2, x3, x4, x5, x6, x7, x8, y5, y6, y7, y8;
 
     /*
      * There's at least one block of 64.
      */
-    x1 = _mm_loadu_si128((__m128i *)(buf + 0x00));
-    x2 = _mm_loadu_si128((__m128i *)(buf + 0x10));
-    x3 = _mm_loadu_si128((__m128i *)(buf + 0x20));
-    x4 = _mm_loadu_si128((__m128i *)(buf + 0x30));
+    x1 = _mm512_loadu_si512((__m512i*)(buf + 0x00));
+    x2 = _mm512_loadu_si512((__m512i*)(buf + 0x10));
+    x3 = _mm512_loadu_si512((__m512i*)(buf + 0x20));
+    x4 = _mm512_loadu_si512((__m512i*)(buf + 0x30));
 
-    x1 = _mm_xor_si128(x1, _mm_cvtsi32_si128(crc));
+    x1 = _mm512_xor_si512(x1, _mm512_castsi128_si512(_mm_cvtsi32_si128(crc)));
 
-    x0 = _mm_load_si128((__m128i *)k1k2);
+    x0 = _mm512_load_si512((__m512i*)k1k2);
 
     buf += 64;
     len -= 64;
@@ -58,30 +58,30 @@ uint32_t ZLIB_INTERNAL crc32_sse42_simd_(  /* SSE4.2+PCLMUL */
      */
     while (len >= 64)
     {
-        x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-        x6 = _mm_clmulepi64_si128(x2, x0, 0x00);
-        x7 = _mm_clmulepi64_si128(x3, x0, 0x00);
-        x8 = _mm_clmulepi64_si128(x4, x0, 0x00);
+        x5 = _mm512_clmulepi64_epi128(x1, x0, 0x00);
+        x6 = _mm512_clmulepi64_epi128(x2, x0, 0x00);
+        x7 = _mm512_clmulepi64_epi128(x3, x0, 0x00);
+        x8 = _mm512_clmulepi64_epi128(x4, x0, 0x00);
 
-        x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-        x2 = _mm_clmulepi64_si128(x2, x0, 0x11);
-        x3 = _mm_clmulepi64_si128(x3, x0, 0x11);
-        x4 = _mm_clmulepi64_si128(x4, x0, 0x11);
+        x1 = _mm512_clmulepi64_epi128(x1, x0, 0x11);
+        x2 = _mm512_clmulepi64_epi128(x2, x0, 0x11);
+        x3 = _mm512_clmulepi64_epi128(x3, x0, 0x11);
+        x4 = _mm512_clmulepi64_epi128(x4, x0, 0x11);
 
-        y5 = _mm_loadu_si128((__m128i *)(buf + 0x00));
-        y6 = _mm_loadu_si128((__m128i *)(buf + 0x10));
-        y7 = _mm_loadu_si128((__m128i *)(buf + 0x20));
-        y8 = _mm_loadu_si128((__m128i *)(buf + 0x30));
+        y5 = _mm512_loadu_si512((__m512i*)(buf + 0x00));
+        y6 = _mm512_loadu_si512((__m512i*)(buf + 0x10));
+        y7 = _mm512_loadu_si512((__m512i*)(buf + 0x20));
+        y8 = _mm512_loadu_si512((__m512i*)(buf + 0x30));
 
-        x1 = _mm_xor_si128(x1, x5);
-        x2 = _mm_xor_si128(x2, x6);
-        x3 = _mm_xor_si128(x3, x7);
-        x4 = _mm_xor_si128(x4, x8);
+        x1 = _mm512_xor_si512(x1, x5);
+        x2 = _mm512_xor_si512(x2, x6);
+        x3 = _mm512_xor_si512(x3, x7);
+        x4 = _mm512_xor_si512(x4, x8);
 
-        x1 = _mm_xor_si128(x1, y5);
-        x2 = _mm_xor_si128(x2, y6);
-        x3 = _mm_xor_si128(x3, y7);
-        x4 = _mm_xor_si128(x4, y8);
+        x1 = _mm512_xor_si512(x1, x5);
+        x2 = _mm512_xor_si512(x2, x6);
+        x3 = _mm512_xor_si512(x3, x7);
+        x4 = _mm512_xor_si512(x4, x8);
 
         buf += 64;
         len -= 64;
@@ -90,34 +90,34 @@ uint32_t ZLIB_INTERNAL crc32_sse42_simd_(  /* SSE4.2+PCLMUL */
     /*
      * Fold into 128-bits.
      */
-    x0 = _mm_load_si128((__m128i *)k3k4);
+    x0 = _mm512_load_si512((__m512i*)k3k4);
 
-    x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-    x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-    x1 = _mm_xor_si128(x1, x2);
-    x1 = _mm_xor_si128(x1, x5);
+    x5 = _mm512_clmulepi64_epi128(x1, x0, 0x00);
+    x1 = _mm512_clmulepi64_epi128(x1, x0, 0x11);
+    x1 = _mm512_xor_si512(x1, x2);
+    x1 = _mm512_xor_si512(x1, x5);
 
-    x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-    x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-    x1 = _mm_xor_si128(x1, x3);
-    x1 = _mm_xor_si128(x1, x5);
+    x5 = _mm512_clmulepi64_epi128(x1, x0, 0x00);
+    x1 = _mm512_clmulepi64_epi128(x1, x0, 0x11);
+    x1 = _mm512_xor_si512(x1, x3);
+    x1 = _mm512_xor_si512(x1, x5);
 
-    x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-    x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-    x1 = _mm_xor_si128(x1, x4);
-    x1 = _mm_xor_si128(x1, x5);
+    x5 = _mm512_clmulepi64_epi128(x1, x0, 0x00);
+    x1 = _mm512_clmulepi64_epi128(x1, x0, 0x11);
+    x1 = _mm512_xor_si512(x1, x4);
+    x1 = _mm512_xor_si512(x1, x5);
 
     /*
      * Single fold blocks of 16, if any.
      */
     while (len >= 16)
     {
-        x2 = _mm_loadu_si128((__m128i *)buf);
+        x2 = _mm512_loadu_si512((__m512i*)buf);
 
-        x5 = _mm_clmulepi64_si128(x1, x0, 0x00);
-        x1 = _mm_clmulepi64_si128(x1, x0, 0x11);
-        x1 = _mm_xor_si128(x1, x2);
-        x1 = _mm_xor_si128(x1, x5);
+        x5 = _mm512_clmulepi64_epi128(x1, x0, 0x00);
+        x1 = _mm512_clmulepi64_epi128(x1, x0, 0x11);
+        x1 = _mm512_xor_si512(x1, x2);
+        x1 = _mm512_xor_si512(x1, x5);
 
         buf += 16;
         len -= 16;
@@ -126,33 +126,33 @@ uint32_t ZLIB_INTERNAL crc32_sse42_simd_(  /* SSE4.2+PCLMUL */
     /*
      * Fold 128-bits to 64-bits.
      */
-    x2 = _mm_clmulepi64_si128(x1, x0, 0x10);
-    x3 = _mm_setr_epi32(~0, 0, ~0, 0);
-    x1 = _mm_srli_si128(x1, 8);
-    x1 = _mm_xor_si128(x1, x2);
+    x2 = _mm512_clmulepi64_epi128(x1, x0, 0x10);
+    x3 = _mm512_setr4_epi32(~0, 0, ~0, 0);
+    x1 = _mm512_srli_epi32(x1, 8);
+    x1 = _mm512_xor_si512(x1, x2);
 
-    x0 = _mm_loadl_epi64((__m128i*)k5k0);
+    x0 = _mm512_load_epi64((__m512i*)k5k0);
 
-    x2 = _mm_srli_si128(x1, 4);
-    x1 = _mm_and_si128(x1, x3);
-    x1 = _mm_clmulepi64_si128(x1, x0, 0x00);
-    x1 = _mm_xor_si128(x1, x2);
+    x1 = _mm512_srli_epi32(x1, 4);
+    x1 = _mm512_and_si512(x1, x3);
+    x1 = _mm512_clmulepi64_epi128(x1, x0, 0x00);
+    x1 = _mm512_xor_si512(x1, x2);
 
     /*
      * Barret reduce to 32-bits.
      */
-    x0 = _mm_load_si128((__m128i*)poly);
+    x0 = _mm512_load_si512((__m512i*)poly);
 
-    x2 = _mm_and_si128(x1, x3);
-    x2 = _mm_clmulepi64_si128(x2, x0, 0x10);
-    x2 = _mm_and_si128(x2, x3);
-    x2 = _mm_clmulepi64_si128(x2, x0, 0x00);
-    x1 = _mm_xor_si128(x1, x2);
+    x2 = _mm512_and_si512(x1, x3);
+    x2 = _mm512_clmulepi64_epi128(x2, x0, 0x10);
+    x2 = _mm512_and_si512(x2, x3);
+    x2 = _mm512_clmulepi64_epi128(x2, x0, 0x00);
+    x1 = _mm512_xor_si512(x1, x2);
 
     /*
      * Return the crc32.
      */
-    return _mm_extract_epi32(x1, 1);
+    return _mm256_extract_epi32(_mm512_castsi512_si256(x1), 1);
 }
 
 #elif defined(CRC32_ARMV8_CRC32)
